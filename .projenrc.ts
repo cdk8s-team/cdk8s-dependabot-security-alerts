e476c3da49d5043bf10c20b99991e2de5e16213e
Vinayak Kukreja <vinakuk@amazon.com>
2023-03-09 22:03:13 -0500

    chore: add cdk8s-dependabot-security-alerts gh action
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,14 @@
-import { GitHubActionTypeScriptProject } from "projen-github-action-typescript";
+import { GitHubActionTypeScriptProject } from 'projen-github-action-typescript';
+
 const project = new GitHubActionTypeScriptProject({
-  defaultReleaseBranch: "main",
-  devDeps: ["projen-github-action-typescript"],
-  name: "cdk8s-dependabot-security-alerts",
+  name: 'cdk8s-dependabot-security-alerts',
+  description: 'CDK8s Github Action for creating issues for dependabot security alerts',
+  defaultReleaseBranch: 'main',
+  devDeps: ['projen-github-action-typescript'],
+  deps: ['@octokit/rest'],
+  bundledDeps: ['@octokit/rest'],
   projenrcTs: true,
   release: false,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
