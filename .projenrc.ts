e58baf09afcccb96d6edea4513c126edfc544ea1
Vinayak Kukreja <vinakuk@amazon.com>
2023-03-09 22:59:38 -0500

    update projen project type to cdk8s typescript project
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,22 +1,35 @@
-import { GitHubActionTypeScriptProject } from 'projen-github-action-typescript';
+// import { GitHubActionTypeScriptProject } from 'projen-github-action-typescript';
+import { Cdk8sTeamTypeScriptProject } from '@cdk8s/projen-common';
 
-const project = new GitHubActionTypeScriptProject({
+// const project = new GitHubActionTypeScriptProject({
+//   name: 'cdk8s-dependabot-security-alerts',
+//   authorName: 'Amazon Web Services',
+//   authorUrl: 'https://aws.amazon.com',
+//   repository: 'https://github.com/cdk8s-team/cdk8s-dependabot-security-alerts',
+//   description: 'CDK8s Github Action for creating issues for dependabot security alerts',
+//   defaultReleaseBranch: 'main',
+//   devDeps: ['projen-github-action-typescript'],
+//   deps: ['@octokit/rest'],
+//   bundledDeps: ['@octokit/rest'],
+//   projenrcTs: true,
+//   release: false,
+//   autoApproveUpgrades: true,
+//   autoApproveOptions: {
+//     allowedUsernames: ['cdk8s-automation'],
+//     secret: 'GITHUB_TOKEN',
+//   },
+// });
+
+const pr = new Cdk8sTeamTypeScriptProject({
   name: 'cdk8s-dependabot-security-alerts',
-  authorName: 'Amazon Web Services',
-  authorUrl: 'https://aws.amazon.com',
-  repository: 'https://github.com/cdk8s-team/cdk8s-dependabot-security-alerts',
   description: 'CDK8s Github Action for creating issues for dependabot security alerts',
   defaultReleaseBranch: 'main',
-  devDeps: ['projen-github-action-typescript'],
-  deps: ['@octokit/rest'],
-  bundledDeps: ['@octokit/rest'],
   projenrcTs: true,
   release: false,
-  autoApproveUpgrades: true,
-  autoApproveOptions: {
-    allowedUsernames: ['cdk8s-automation'],
-    secret: 'GITHUB_TOKEN',
-  },
+  devDeps: ['@cdk8s/projen-common'],
+  deps: ['@octokit/rest'],
+  bundledDeps: ['@octokit/rest'],
 });
 
-project.synth();
+pr.synth();
+
