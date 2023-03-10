0f4ce226ad7f35a97e50620b88e5b79be3086262
Vinayak Kukreja <vinakuk@amazon.com>
2023-03-09 22:28:00 -0500

    add projen items
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,6 +2,9 @@ import { GitHubActionTypeScriptProject } from 'projen-github-action-typescript';
 
 const project = new GitHubActionTypeScriptProject({
   name: 'cdk8s-dependabot-security-alerts',
+  authorName: 'Amazon Web Services',
+  authorUrl: 'https://aws.amazon.com',
+  repository: 'https://github.com/cdk8s-team/cdk8s-dependabot-security-alerts',
   description: 'CDK8s Github Action for creating issues for dependabot security alerts',
   defaultReleaseBranch: 'main',
   devDeps: ['projen-github-action-typescript'],
@@ -9,6 +12,11 @@ const project = new GitHubActionTypeScriptProject({
   bundledDeps: ['@octokit/rest'],
   projenrcTs: true,
   release: false,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ['cdk8s-automation'],
+    secret: 'GITHUB_TOKEN',
+  },
 });
 
 project.synth();
